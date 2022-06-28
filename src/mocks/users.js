0162c2dc3ff900e993a782faa7fde760daa7d814
Eric Antunes <antunesv.eric@gmail.com>
2022-06-27 23:23:58 -0300

    commit inicial

---
+++ src/mocks/users.js
@@ -0,0 +1,15 @@
+module.exports = [{
+        id: 1,
+        name: 'Eric',
+    },
+
+    {
+        id: 2,
+        name: 'Vieira',
+    },
+
+    {
+        id: 3,
+        name: 'Antunes',
+    },
+];
